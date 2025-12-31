081ba8ef628d308b38bf3e7b1c60fcaf7fb44cdc
Saatvik Chauhan <saatvikchauhan03@gmail.com>
2025-12-31 09:04:37 +0530

    Ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -261,4 +261,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000, () => console.log("🎅 Server running on port 4000"));
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`🎅 Server running on port ${PORT}`);
+});
